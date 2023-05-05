129db4870c3d7f68b4079e49b592f0eb11e98ab5
Benjamin Cortes <benjamca@hotmail.com>
2023-05-05 16:58:04 -0400

    Realizando el save de usuario

--- controllers/user.js
+++ controllers/user.js
@@ -1,5 +1,8 @@
+// Importar dependencias y modulos
+const User = require("../models/user");
+const bcrypt = require("bcrypt");
 
-//Acciones de prueba
+//Acciones de prueba 
 const pruebaUser = (req,res) => {
     return res.status(200).send(
         {
@@ -11,23 +14,72 @@ const pruebaUser = (req,res) => {
 //****************METODOS****************
 
     //Registro de usuarios
-    const register = (req,res) => {
+    const register = async (req,res) => {
         // Recoger datos de la peticion
-
+        let params = req.body;
         //Comprobar que me llegan bien y validarlos
+        if(!params.name || !params.nick || !params.email || !params.password){
+            console.log("VALIDACION INCORRECTA");
+            
+            return res.status(400).json(
+                {
+                    message: "Faltan datos por enviar",
+                    status: "error"
+                }
+            )
+        }
+        
+       
+
 
         //Control de usuarios duplicados
+         User.find(  { $or: [
+        {email: params.email.toLowerCase()},
+        {nick: params.nick.toLowerCase()}
+       ]}).then(  (users) =>  {
 
-        // Cifrar la contraseña
+        if (!users) {
+            return res.status(500).json(
+                {
+                    message: "Error en la consulta",
+                    status: "error"
+                }
+            )
+        }
 
+        if (users && users.length >= 1) {
+            return res.status(200).send({
+                message: "El usuario ya existe",
+                status: "success",
+            })
+        }
+       })
+
+        // Cifrar la contraseña 
+       let pwd = await bcrypt.hash(params.password, 10); // el 10 es cuantas veces cifra la contraseña
+       params.password = pwd;
+
+        // Crear objeto de usuario
+        let user_to_save = new User(params);
+       
         // Guardar usuario en la base de datos
+        user_to_save.save().then((userStored) => {
+            if (!userStored) {
+                console.log("error");
+            }else{
+                console.log("registrado");
+            }
+        });
+      
 
         // Devolver resultado
 
 
         return res.status(200).json(
             {
-                message: "Registro de nuevo usuario"
+                message: "Registro de nuevo usuario",
+                status: "success",
+                user_to_save
             }
         )
     }
--- routes/user.js
+++ routes/user.js
@@ -4,8 +4,8 @@ const router = express.Router();
 const UserController = require("../controllers/user");
 
 // Definir rutas
-router.get("/prueba-usuario",UserController.pruebaUser);
-router.get("/register",UserController.register);
+router.post("/prueba-usuario",UserController.pruebaUser);
+router.post("/register",UserController.register);
 
 // Exportar router
 module.exports = router;
